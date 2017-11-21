393642ef3c797ec2d45670b47dac1c23cf1bc567
Stefan Thomas <justmoon@members.fsf.org>
2017-11-21 14:16:12 -0800

    feat: pass fulfillment data back

--- src/handlers/private/fulfill-condition.js
+++ src/handlers/private/fulfill-condition.js
@@ -1,5 +1,5 @@
 'use strict'
 
-module.exports = ({ plugin }) => async ({ id, fulfillment }) => {
-  await plugin.fulfillCondition(id, fulfillment)
+module.exports = ({ plugin }) => async ({ id, fulfillment, fulfillmentData }) => {
+  await plugin.fulfillCondition(id, fulfillment, fulfillmentData)
 }
